67fbc49b2971fd956e068f97656601af157d957c
1Kuso4ek1 <dimasalov2006@gmail.com>
2025-08-03 19:36:06 +0300

    User profile update, avatar upload

--- include/Controllers/UsersController.hpp
+++ include/Controllers/UsersController.hpp
@@ -13,12 +13,16 @@ class UsersController final : public HttpController<UsersController>
 public:
     static void getUserById(const HttpRequestPtr& req, Callback&& callback, int id);
     static void getMe(const HttpRequestPtr& req, Callback&& callback);
+    static void updateMe(const HttpRequestPtr& req, Callback&& callback);
+    static void uploadAvatar(const HttpRequestPtr& req, Callback&& callback);
 
 public:
     METHOD_LIST_BEGIN
 
         ADD_METHOD_TO(getUserById, "/api/users/{id}", Get, "Filters::JwtFilter");
         ADD_METHOD_TO(getMe, "/api/users/me", Get, "Filters::JwtFilter");
+        ADD_METHOD_TO(updateMe, "/api/users/me", Post, "Filters::JwtFilter");
+        ADD_METHOD_TO(uploadAvatar, "/api/users/me/avatar", Post, "Filters::JwtFilter");
 
     METHOD_LIST_END
 };
--- src/Controllers/UsersController.cpp
+++ src/Controllers/UsersController.cpp
@@ -45,4 +45,82 @@ void UsersController::getMe(const HttpRequestPtr& req, Callback&& callback)
     }
 }
 
+void UsersController::updateMe(const HttpRequestPtr& req, Callback&& callback)
+{
+    static auto mapper = orm::Mapper<Models::Users>(app().getDbClient());
+
+    const auto selfId = std::stoi(req->getParameter("user_id"));
+    const auto json = *req->getJsonObject();
+
+    if(!json.isMember("display_name") || !json.isMember("username")
+        || json["username"].asString().find(' ') != std::string::npos)
+    {
+        callback(HttpResponse::newHttpResponse(k400BadRequest, CT_NONE));
+        return;
+    }
+
+    try
+    {
+        auto user = mapper.findByPrimaryKey(selfId);
+        user.setDisplayName(json["display_name"].asString());
+        user.setUsername(json["username"].asString());
+
+        mapper.update(user);
+
+        callback(HttpResponse::newHttpJsonResponse(user.toJson()));
+    }
+    catch(...)
+    {
+        callback(HttpResponse::newHttpResponse(k409Conflict, CT_NONE));
+    }
+}
+
+void UsersController::uploadAvatar(const HttpRequestPtr& req, Callback&& callback)
+{
+    static auto mapper = orm::Mapper<Models::Users>(app().getDbClient());
+
+    const auto selfId = std::stoi(req->getParameter("user_id"));
+
+    MultiPartParser parser;
+    if(parser.parse(req) != 0)
+    {
+        callback(HttpResponse::newHttpResponse(k400BadRequest, CT_NONE));
+        return;
+    }
+
+    const auto& files = parser.getFiles();
+    if(files.size() != 1)
+    {
+        callback(HttpResponse::newHttpResponse(k400BadRequest, CT_NONE));
+        return;
+    }
+
+    const auto& file = files[0];
+    const auto filename = std::format("{}.{}", file.getMd5(), file.getFileExtension());
+    const auto url = std::format(
+        "{}://{}/img/{}",
+        req->getConnectionPtr().lock()->isSSLConnection() ? "https" : "http",
+        req->getHeader("host"),
+        filename
+    );
+
+    try
+    {
+        auto user = mapper.findByPrimaryKey(selfId);
+        user.setAvatarUrl(url);
+
+        mapper.update(user);
+
+        file.saveAs(std::format("{}/img/{}", app().getDocumentRoot(), filename));
+
+        callback(HttpResponse::newHttpJsonResponse(user.toJson()));
+    }
+    catch(...)
+    {
+        LOG_ERROR << "Failed to get user by selfId: " << selfId;
+
+        callback(HttpResponse::newHttpResponse(k404NotFound, CT_NONE));
+    }
+}
+
 }
